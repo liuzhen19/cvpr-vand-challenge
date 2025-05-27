c4562b1818232f8258e9b0bd8ad8aacf252c6432
liuzhen <liuzhen_dw@163.com>
2025-05-27 23:38:24 +0800

    fastlogsadv2.0

--- src/eval/submission/model.py
+++ src/eval/submission/model.py
@@ -1,8 +1,7 @@
-"""Model for submission."""  # add BeiT for optimization
+"""Model for submission."""
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
-
 
 import torch
 from anomalib.data import ImageBatch
